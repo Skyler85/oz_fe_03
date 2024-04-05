4862824f87f9dccbdaee40ecea9425fef42457d6
Skyler85 <worstlie1@gmail.com>
2024-04-05 16:43:46 +0900

    Javascript - challenge 04. 강의 소스 업로드 - 11일차 학습내용 & miniProject - 로또번호생성기 > element 생성 및 버튼이벤트 > 랜덤함수 이용하여 무작위의 숫자 1 ~ 45 중 6개의 숫자 배열에 담아 출력하기

---
+++ javascript/mini_project/11day_lotto.js
@@ -0,0 +1,39 @@
+// '추첨' 버튼을 누른 후, '다시' 버튼을 누르지 않은 채 '추첨' 버튼을 또 눌러도 새롭게 번호가 추첨되도록 기능을 수정해보자!
+
+const todaySpan = document.querySelector('#today');
+const numbersDiv = document.querySelector('.numbers');
+const drawButton = document.querySelector('#draw');
+const resetButton = document.querySelector('#reset');
+let lottoNumbers = [];
+
+// 오늘 날짜 가져오기
+const today = new Date();
+let year = today.getFullYear();
+let month = today.getMonth() + 1;
+let date = today.getDate();
+todaySpan.textContent = `${year}년 ${month}월 ${date}일 `;
+
+// paintNumber 함수
+function paintNumber(number){
+    const eachNumDiv = document.createElement("div");
+    eachNumDiv.classList.add("eachnum");
+    eachNumDiv.textContent = number;
+    numbersDiv.appendChild(eachNumDiv)
+};
+
+// 클릭하면 랜덤 숫자 여섯개가 배열에 추가된다
+drawButton.addEventListener('click', function(){
+    while(lottoNumbers.length < 6){
+        let rn = Math.floor(Math.random() * 45) + 1;
+
+        if(lottoNumbers.indexOf(rn) === -1){
+            lottoNumbers.push(rn);
+            paintNumber(rn);
+        }
+    }
+})
+
+resetButton.addEventListener('click', function(){
+    lottoNumbers.slice(0, 6);
+    numbersDiv.innerHTML = "";
+})

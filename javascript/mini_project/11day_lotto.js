3af81bbdc377ee3ee680e8b7d358843da248f247
Skyler85 <worstlie1@gmail.com>
2024-04-13 03:38:03 +0900

    Javascript - challenge 04. 11일차 miniProject - 로또번호 생성 > js코드 일부 수정 > 버튼 이벤트 내부의 랜덤숫자 생성 함수를 전역레벨에서 별도의 함수로 분리하여 호출함.

--- javascript/mini_project/11day_lotto.js
+++ javascript/mini_project/11day_lotto.js
@@ -1,9 +1,9 @@
 // '추첨' 버튼을 누른 후, '다시' 버튼을 누르지 않은 채 '추첨' 버튼을 또 눌러도 새롭게 번호가 추첨되도록 기능을 수정해보자!
 
-const todaySpan = document.querySelector('#today');
-const numbersDiv = document.querySelector('.numbers');
-const drawButton = document.querySelector('#draw');
-const resetButton = document.querySelector('#reset');
+const todaySpan = document.querySelector("#today");
+const numbersDiv = document.querySelector(".numbers");
+const drawButton = document.querySelector("#draw");
+const resetButton = document.querySelector("#reset");
 let lottoNumbers = [];
 
 // 오늘 날짜 가져오기
@@ -14,26 +14,38 @@ let date = today.getDate();
 todaySpan.textContent = `${year}년 ${month}월 ${date}일 `;
 
 // paintNumber 함수
-function paintNumber(number){
-    const eachNumDiv = document.createElement("div");
-    eachNumDiv.classList.add("eachnum");
-    eachNumDiv.textContent = number;
-    numbersDiv.appendChild(eachNumDiv)
-};
+function paintNumber(number) {
+  const eachNumDiv = document.createElement("div");
+  eachNumDiv.classList.add("eachnum");
+  eachNumDiv.textContent = number;
+  numbersDiv.appendChild(eachNumDiv);
+}
+
+// 랜덤 숫자 배열 생성
+function makeNum() {
+  if (lottoNumbers.length !== 0) {
+    () => lottoNumbers.slice(0, 6);
+    numbersDiv.innerHTML = "";
+  }
+
+  for (let i = 0; i < 6; i++) {
+    let rn = Math.floor(Math.random() * 45) + 1;
+    for (let j in lottoNumbers) {
+      while (rn == lottoNumbers[j]) {
+        let rn = Math.floor(Math.random() * 45) + 1;
+        lottoNumbers.push(rn);
+        paintNumber(rn);
+      }
+    }
+  }
+}
+if (lottoNumbers.indexOf(rn) === -1) {
+}
 
 // 클릭하면 랜덤 숫자 여섯개가 배열에 추가된다
-drawButton.addEventListener('click', function(){
-    while(lottoNumbers.length < 6){
-        let rn = Math.floor(Math.random() * 45) + 1;
+drawButton.addEventListener("click", makeNum());
 
-        if(lottoNumbers.indexOf(rn) === -1){
-            lottoNumbers.push(rn);
-            paintNumber(rn);
-        }
-    }
-})
-
-resetButton.addEventListener('click', function(){
-    lottoNumbers.slice(0, 6);
-    numbersDiv.innerHTML = "";
-})
+resetButton.addEventListener("click", function () {
+  lottoNumbers.slice(0, 6);
+  numbersDiv.innerHTML = "";
+});

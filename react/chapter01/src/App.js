d6155268f1435df046f61b5476ba9da847185f14
Skyler85 <worstlie1@gmail.com>
2024-05-07 22:35:11 +0900

    react - chapter01. 15~18 react-icons 설치 및 JSX Key, Props 사용해보기

--- react/chapter01/src/App.js
+++ react/chapter01/src/App.js
@@ -16,8 +16,16 @@ import './App.css';
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
 
-
 export default class App extends Component {
+    initialExpenses = [
+        { id: 1, charge: '콜라', amount: 2000 },
+        { id: 2, charge: '빵', amount: 1000 },
+        { id: 3, charge: '맥북', amount: 20000 },
+    ]
+
+    handleDelete = (id) => {
+        const newExpense = this.initialExpenses.filter(expense => expense.id === id)
+    }
     render() {
         return (
             <main className='main-container'>
@@ -27,7 +35,7 @@ export default class App extends Component {
                       <ExpenseForm />
                       </div>
                     <div style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}>
-                      <ExpenseList />
+                      <ExpenseList initialExpenses={this.initialExpenses} handleDelete={this.handleDelete} />
                       </div>
                     <div style={{ display: "flex", justifyContent: "start", marginTop: "1rem" }}>
                         <p style={{ fontSize: "2rem", color: "" }}>총합계:</p>
